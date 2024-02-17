510b08a124d3a6570532da74662160b4a7a7e229
pejir <pejir84@gmail.com>
2024-02-17 19:04:24 +0100

    Update 1_unit-tests.js

--- freeCodeCamp (🔥)/Quality Assurance and Testing with Chai/boilerplate-mochachai-main/tests/1_unit-tests.js
+++ freeCodeCamp (🔥)/Quality Assurance and Testing with Chai/boilerplate-mochachai-main/tests/1_unit-tests.js
@@ -4,8 +4,11 @@ const assert = chai.assert;
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+    test('#isNull, #isNotNull', function() {
+      assert.isNull(
+        null,
+        'this is an optional error description - e.g. null is null'
+      );
       assert.isNotNull(1, '1 is not null');
     });
     // #2
