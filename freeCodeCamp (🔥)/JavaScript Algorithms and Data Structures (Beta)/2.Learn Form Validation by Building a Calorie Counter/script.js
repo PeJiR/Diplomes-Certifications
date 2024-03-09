99746b9176a1a353d80d02efcf252da380296a36
pejir <pejir84@gmail.com>
2024-03-09 18:00:46 +0100

    Update script.js

--- freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/2.Learn Form Validation by Building a Calorie Counter/script.js
+++ freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/2.Learn Form Validation by Building a Calorie Counter/script.js
@@ -32,6 +32,33 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+  if (isError) {
+    return;
+  }
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories <0? "Surplus":"Deficit";
+
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
@@ -40,10 +67,13 @@ function getCaloriesFromInputs(list) {
     const invalidInputMatch = isInvalidInput(currVal);
 
     if (invalidInputMatch) {
-      alert(`Invalid Input: ${invalidInputMatch[0]}`)
-
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
     }
+    calories += Number(currVal);
   }
+  return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
