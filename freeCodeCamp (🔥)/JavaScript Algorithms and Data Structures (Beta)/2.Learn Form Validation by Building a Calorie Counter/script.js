4bd9996ea4cb31a6880bf1aeab5030a7e7e7855d
pejir <pejir84@gmail.com>
2024-03-09 18:20:07 +0100

    Update script.js

--- freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/2.Learn Form Validation by Building a Calorie Counter/script.js
+++ freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/2.Learn Form Validation by Building a Calorie Counter/script.js
@@ -55,8 +55,10 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories <0? "Surplus":"Deficit";
-
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit} </span>
+  `;
 }
 
 function getCaloriesFromInputs(list) {
