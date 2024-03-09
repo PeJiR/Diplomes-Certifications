8a9c917e43c6e5dff07bb8c95fee9ac506ef30ae
pejir <pejir84@gmail.com>
2024-03-09 01:19:51 +0100

    Update script.js

--- freeCodeCamp (🔥)/Learn Basic JavaScript by Building a Role Playing Game/Learn Basic JavaScript by Building a Role Playing Game/script.js
+++ freeCodeCamp (🔥)/Learn Basic JavaScript by Building a Role Playing Game/Learn Basic JavaScript by Building a Role Playing Game/script.js
@@ -19,5 +19,7 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
 }
