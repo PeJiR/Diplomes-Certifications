0fe3a09758798ce9e949b70b1468c1364c72a21c
pejir <pejir84@gmail.com>
2024-03-15 20:59:06 +0100

    update

--- Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
+++ Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
@@ -60,6 +60,62 @@ a = 20
 b = 5
 c = a / b
 type(c)
-    
+
+""" 
+Roberto Perez
+Date : mars 2024
+but du programme
+
+"""
 
 #2.5.1. Manipuler des scripts avec PyCharm
+#lecture du rayon 
+rayon = float(input("Veuillez donner le rayon : "))  # lecture du rayon de mon disque
+circ = 2 * 3.14  * rayon                    # calcul de la circonférence
+aire = 3.14 * rayon **  2                   # calcul de l'aire
+# Affichage des résultats
+print("Circonférence : ", circ)
+print("Aire          : ", aire)
+
+
+
+
+
+
+
+"""Auteur: Thierry Massart
+   Date : 5 décembre 2017
+   But du programme : calcule les ingrédients nécessaires
+   pour préparer de la mousse au chocolat pour n personnes
+   Entrée: n (nombre de personnes)
+   Sorties: nombre d'oeufs,
+            quantité en gramme de chocolat,
+            nombre de sachets de sucre vanillé
+"""
+n = int(input(5))     # entrée: nombre de personnes
+print("nombre d'oeufs : ", int(3 * n / 4)) # calcule et affiche les résultats
+print("quantité de chocolat en grammes : ", int(100 * n / 4))
+print("quantité de sucre_vanillé : ", max(int(n / 4), 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
