16a21d6424b076f8f8aeef6d972e7824934cbd3f
pejir <pejir84@gmail.com>
2024-03-15 19:52:26 +0100

    Update def ma_fonction(x=2):.py

--- Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
+++ Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
@@ -10,3 +10,21 @@ def ma_fonction(x=2):
 
 10%3
 10//3
+
+5-3-2
+
+2 ** (2 ** 3)
+
+2 + 3 * 4 ** 5
+
+-8 % 3
+
+8 % -3
+
+# Quiz : question 1 
+int(7*3/4)
+
+# Quiz : question 2 Pour faire de la mousse pour 7 personnes, combien de grammes de chocolat vous faut-il ? On donnera la valeur tronquée à l'unité. 
+int(7*3/4)
+
+
