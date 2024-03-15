cc0f9ae5249254435aaed020c9abefdd733cb81e
pejir <pejir84@gmail.com>
2024-03-15 20:16:52 +0100

    Update def ma_fonction(x=2):.py

--- Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
+++ Fun-mooc.fr/Apprendre a coder avec Python/def ma_fonction(x=2):.py
@@ -29,3 +29,30 @@ int(7*100/4)
 
 #Quiz : question 3 Pour faire de la mousse pour 7 personnes, combien de sachets de sucre vanillé vous faut-il ? On donnera la valeur tronquée à l'unité. 
 int(7*1/4)
+
+type('bonjour2.')
+'hola' + '   robe'
+
+# Quiz sur la fonction len() - question 1 
+len("C'est facile de compter") 
+len('123' * 20) 
+len('' * 20) 
+
+# 2.4.1. Les variables
+
+x=3
+x
+
+y = 4*3
+y
+
+x+ y 
+
+r =8/3
+r 
+type(r)
+type(x)
+
+
+
+
