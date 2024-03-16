04d28adb4dc55309743a1e3e2b958cd6b3e5fe89
pejir <pejir84@gmail.com>
2024-03-17 00:50:34 +0100

    Update script.js

--- freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/8.Learn Recursion by Building a Decimal to Binary Converter/script.js
+++ freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/8.Learn Recursion by Building a Decimal to Binary Converter/script.js
@@ -2,22 +2,21 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+
+
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = "";
 
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
+  if (input === 0) {
+    binary = "0";
   }
 
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
