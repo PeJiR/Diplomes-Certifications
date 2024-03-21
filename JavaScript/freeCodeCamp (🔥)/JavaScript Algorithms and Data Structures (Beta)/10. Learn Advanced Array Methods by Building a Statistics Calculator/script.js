4f539e52b9bd12419ce9573af32bdc6f8f223751
pejir <pejir84@gmail.com>
2024-03-21 20:18:59 +0100

    Update script.js

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -1,7 +1,13 @@
-const getMean = (array) =>{}
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian =(array)=>{}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
 }
