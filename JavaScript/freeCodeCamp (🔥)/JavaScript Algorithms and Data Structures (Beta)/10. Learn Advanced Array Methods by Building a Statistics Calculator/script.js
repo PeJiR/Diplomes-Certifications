95580e2c3a8c2b52c409df3d83db47af5f6d2328
pejir <pejir84@gmail.com>
2024-03-25 23:16:50 +0100

    update

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -32,8 +32,12 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
-  const differences = array.map(el => el -mean );
+  const variance = array.reduce((acc, el) => {
+    const difference = el- mean;
+    const squared = difference**2;
+    return acc + squared
 
+  }, 0);
 }
 
 const calculate = () => {
