44f840281e646efbdc76098e7959bdd480c4fe22
pejir <pejir84@gmail.com>
2024-03-25 23:37:45 +0100

    update

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/10. Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -1,7 +1,7 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.slice().sort((a, b) => a - b);
   const median =
     array.length % 2 === 0
       ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
@@ -33,25 +33,35 @@ const getRange = (array) => {
 const getVariance = (array) => {
   const mean = getMean(array);
   const variance = array.reduce((acc, el) => {
-    const difference = el- mean;
-    const squared = difference**2;
-    return acc + squared
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
 
-  }, 0);
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
 }
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
   const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
 }
