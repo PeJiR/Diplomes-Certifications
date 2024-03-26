9752a2637443649b53d42a404fb6b7b3f0e7c416
pejir <pejir84@gmail.com>
2024-03-26 23:45:41 +0100

    Update script.js

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
@@ -1,3 +1,18 @@
+const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+
+const median = nums => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
+
+
+
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
@@ -13,9 +28,12 @@ window.onload = () => {
   letters.forEach(createLabel);
   range(1, 99).forEach(number => {
     createLabel(number);
-    letters.forEach(letter =>{
-
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
     })
-
   })
 }
