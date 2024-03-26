86f2c144f24adeddc1bed05d5e6bff7b1ee1e0e9
pejir <pejir84@gmail.com>
2024-03-26 23:16:05 +0100

    Update script.js

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
@@ -1,5 +1,5 @@
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
-const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code =>String.fromCharCode(code));
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -9,4 +9,13 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   }
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter =>{
+
+    })
+
+  })
 }
