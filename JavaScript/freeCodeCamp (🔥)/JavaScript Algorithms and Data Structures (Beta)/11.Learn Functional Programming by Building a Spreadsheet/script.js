66562e8d30c11ba2f95f04b71a8e26c093d6bd97
pejir <pejir84@gmail.com>
2024-03-27 23:54:56 +0100

    Update script.js

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
@@ -20,6 +20,15 @@ const spreadsheetFunctions = {
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])/;
+
+
+
+
+}
+
 window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = (name) => {
@@ -46,9 +55,7 @@ window.onload = () => {
 const update = event => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
-  if (!value.includes(element.id)){
+  if (!value.includes(element.id) && value.startsWith('=')) {
 
   }
-
-
 }
