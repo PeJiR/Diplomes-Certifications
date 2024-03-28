123f2e43db6ecf3b774b58622d908fd6cc2fcc9e
pejir <pejir84@gmail.com>
2024-03-29 00:33:41 +0100

    Update script.js

--- JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
+++ JavaScript/freeCodeCamp (🔥)/JavaScript Algorithms and Data Structures (Beta)/11.Learn Functional Programming by Building a Spreadsheet/script.js
@@ -24,17 +24,9 @@ const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-  const elemValue = num => {
-    const inner = character => {
-      return idToText(character + num);
-    }
-    return inner;
-  }
-  const addCharacters = character1 => character2 => num =>charRange(character1,character2); {
-    
-    
-
-  }
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(rangeRegex, (match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)));
 }
 
 window.onload = () => {
